be4d9dd69162b97e75a8f418678a9c87e24b3a0c
Harika K <harika@swaas.local>
2018-07-05 18:15:38 +0530

    Appending to previous commit

--- app/src/main/java/com/swaas/mwc/Fragments/LoginAgreeTermsAcceptanceFragment.java
+++ app/src/main/java/com/swaas/mwc/Fragments/LoginAgreeTermsAcceptanceFragment.java
@@ -1,13 +1,16 @@
 package com.swaas.mwc.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.swaas.mwc.Login.LoginAgreeTermsAcceptanceActivity;
+import com.swaas.mwc.Login.LoginHelpUserGuideActivity;
 import com.swaas.mwc.R;
 
 /**
@@ -18,6 +21,7 @@ public class LoginAgreeTermsAcceptanceFragment extends Fragment{
 
     LoginAgreeTermsAcceptanceActivity mActivity;
     View mView;
+    Button mIAgreeButton;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +34,24 @@ public class LoginAgreeTermsAcceptanceFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.login_terms_acceptance_fragment, container, false);
 
+        intializeViews();
+        addListenersToViews();
         return mView;
+    }
+
+    private void intializeViews() {
+
+        mIAgreeButton = (Button) mView.findViewById(R.id.agree_button);
+    }
+
+    private void addListenersToViews() {
+
+        mIAgreeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mActivity, LoginHelpUserGuideActivity.class));
+                mActivity.finish();
+            }
+        });
     }
 }
