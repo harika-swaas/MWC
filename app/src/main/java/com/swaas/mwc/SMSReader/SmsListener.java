97bf8b80747f419914918ac41271750385847755
Jayaraman D <jayaraman@swaas.local>
2018-08-30 09:33:37 +0530

    PackageName changed

--- app/src/main/java/com/swaas/mwc/SMSReader/SmsListener.java
+++
@@ -1,9 +0,0 @@
-package com.swaas.mwc.SMSReader;
-
-/**
- * Created by harika on 12-07-2018.
- */
-
-public interface SmsListener{
-    public void messageReceived(String messageText);
-}
--- app/src/main/java/com/swaas/mwc/Utils/ClickableTextUtil.java
+++
@@ -1,8 +0,0 @@
-package com.swaas.mwc.Utils;
-
-/**
- * Created by harika on 29-06-2018.
- */
-
-
-
