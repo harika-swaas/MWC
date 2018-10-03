56057c7e3dc4b2d70e8ac6330def8345eddff40d
Jayaraman D <jayaraman@swaas.local>
2018-10-03 10:49:37 +0530

    missing files added

---
+++ app/src/main/java/com/mwc/docportal/GridAutofitLayoutManager.java
@@ -0,0 +1,17 @@
+package com.mwc.docportal;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+
+public class GridAutofitLayoutManager{
+    public static int calculateNoOfColumns(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int noOfColumns = (int) (dpWidth / 100);
+        return noOfColumns;
+    }
+}
